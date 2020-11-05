d88a611c446a5db2ec4cd2f6b86092018352bd27
Mikkel Christensen <mikkel@ebi.ac.uk>
2020-11-05 15:53:03 +0000

    adding REST API and test. Modification to schema

---
+++ session_service/rest_api.py
@@ -0,0 +1,265 @@
+from sqlalchemy.orm import Session as SqlSession
+from sqlalchemy.exc import SQLAlchemyError
+
+
+class AssigningApplication:
+
+    def __init__(self, sql_alchemy_base, sql_alchemy_engine):
+        self.assigning_application = sql_alchemy_base.classes.assigning_application
+        self.sql_session = SqlSession(sql_alchemy_engine)
+
+    def __del__(self):
+            self.sql_session.close()
+
+    def get(self, **kwargs):
+
+        if 'application_id' in kwargs:
+            result = self.sql_session.query(self.assigning_application).get(kwargs['application_id'])
+            if result is not None:
+                return result.name, int(result.version), result.description
+            else:
+                return False
+        elif 'name' in kwargs and 'version' in kwargs:
+            result = self.sql_session.query(self.assigning_application).filter(
+                self.assigning_application.name == kwargs['name'],
+                self.assigning_application.version == kwargs['version'])
+            return result[0].application_id
+        else:
+            return False  # 400 Bad Request
+
+    def post(self, **kwargs):
+        if 'name' in kwargs and 'version' in kwargs and 'description' in kwargs:
+            new_application = self.assigning_application(name=kwargs['name'], version=kwargs['version'],
+                                                         description=kwargs['description'])
+            self.sql_session.add(new_application)
+            try:
+                self.sql_session.commit()
+            except SQLAlchemyError as mysql_error:
+                print(mysql_error.__str__())
+                return False
+            return new_application.application_id
+        else:
+            return False  # 400 Bad Request
+
+    def patch(self, **kwargs):
+        if 'application_id' in kwargs and 'description' in kwargs:
+            row = self.sql_session.query(self.assigning_application).get(kwargs['application_id'])
+            if row is not None:
+                row.description = kwargs['description']
+            else:
+                return False
+            try:
+                self.sql_session.commit()
+                return True
+            except SQLAlchemyError as mysql_error:
+                print(mysql_error.__str__())
+                return False
+        else:
+            return False  # 400 Bad Request
+
+    @staticmethod
+    def delete():
+        return False  # 405 METHOD NOT ALLOWED
+
+
+class ProductionDatabase:
+
+    def __init__(self, sql_alchemy_base, sql_alchemy_engine):
+        self.production_database = sql_alchemy_base.classes.production_database
+        self.sql_session = SqlSession(sql_alchemy_engine)
+
+    def __del__(self):
+        self.sql_session.close()
+
+    def get(self, **kwargs):
+        if 'production_database_id' in kwargs:
+            result = self.sql_session.query(self.production_database).get(kwargs['production_database_id'])
+            if result is not None:
+                return result.name
+            else:
+                return False
+        else:
+            return False  # 400 Bad Request
+
+    def post(self, **kwargs):
+        if 'name' in kwargs:
+            new_database = self.production_database(name=kwargs['name'])
+            self.sql_session.add(new_database)
+            try:
+                self.sql_session.commit()
+            except SQLAlchemyError as mysql_error:
+                print(mysql_error.__str__())
+                return False
+            return new_database.production_database_id
+        else:
+            return False  # 400 Bad Request
+
+    def patch(self, **kwargs):
+        if 'production_database_id' in kwargs and 'name' in kwargs:
+
+            row = self.sql_session.query(self.production_database).get(kwargs['production_database_id'])
+            if row is not None:
+                row.name = kwargs['name']
+            else:
+                return False
+            try:
+                self.sql_session.commit()
+                return True
+            except SQLAlchemyError as mysql_error:
+                print(mysql_error.__str__())
+                return False
+        else:
+            return False  # 400 Bad Request
+
+    @staticmethod
+    def delete():
+        return False  # 405 METHOD NOT ALLOWED
+
+
+class Session:
+
+    def __init__(self, sql_alchemy_base, sql_alchemy_engine):
+        self.session_table = sql_alchemy_base.classes.session
+        self.sql_session = SqlSession(sql_alchemy_engine)
+
+    def __del__(self):
+        self.sql_session.close()
+
+    def get(self, **kwargs):
+        if 'session_id' in kwargs:
+            result = self.sql_session.query(self.session_table).get(kwargs['session_id'])
+            return result.ses_application_id, result.ses_production_database_id, result.osid_idsetid,\
+                result.data_check, result.message, result.creation_date
+        else:
+            return False  # 400 Bad Request
+
+    def post(self, **kwargs):
+        if 'application_id' in kwargs and 'production_database_id' in kwargs and 'osid_idsetid' in kwargs\
+                and 'message' in kwargs:
+            new_session = self.session_table(ses_application_id=kwargs['application_id'],
+                                             ses_production_database_id=kwargs['production_database_id'],
+                                             osid_idsetid=kwargs['osid_idsetid'], message=kwargs['message'])
+            self.sql_session.add(new_session)
+            try:
+                self.sql_session.commit()
+            except SQLAlchemyError as mysql_error:
+                print(mysql_error.__str__())
+                return False
+            return new_session.session_id
+        else:
+            return False  # '400 Bad Request'
+
+    def patch(self, **kwargs):
+        if kwargs['session_id'] and kwargs['data_check']:
+            row = self.sql_session.query(self.session_table).get(kwargs['session_id'])
+            row.data_check = kwargs['data_check']
+            try:
+                self.sql_session.commit()
+                return True
+            except SQLAlchemyError as mysql_error:
+                print(mysql_error.__str__())
+                return False
+        else:
+            return False  # 400 Bad Request
+
+    @staticmethod
+    def delete():
+            return False  # 400 Bad Request
+
+
+class SessionIdentifierAction:
+
+    def __init__(self, sql_alchemy_base, sql_alchemy_engine):
+        self.session_identifier_action = sql_alchemy_base.classes.session_identifier_action
+        self.sql_session = SqlSession(sql_alchemy_engine)
+
+    def __del__(self):
+        self.sql_session.close()
+
+    def get(self, **kwargs):
+        if 'session_identifier_action_id' in kwargs:
+            result = self.sql_session.query(self.session_identifier_action).get(kwargs['session_identifier_action_id'])
+            return result.sia_stable_identifier_record_id, result.sia_session_id, result.action
+        else:
+            return False  # 400 Bad Request
+
+    def post(self, **kwargs):
+        if 'stable_identifier_record_id' in kwargs and 'session_id' in kwargs and 'action' in kwargs:
+            new_session_identifier_action = self.session_identifier_action(
+                sia_stable_identifier_record_id=kwargs['stable_identifier_record_id'], sia_session_id=kwargs['session_id'],
+                action=kwargs['action'])
+            self.sql_session.add(new_session_identifier_action)
+            try:
+                self.sql_session.commit()
+            except SQLAlchemyError as mysql_error:
+                print(mysql_error.__str__())
+                return False
+            return new_session_identifier_action.session_identifier_action_id
+        else:
+            return False  # 400 Bad Request
+
+    def patch(self, **kwargs):
+        if 'session_identifier_action_id' in kwargs and 'action' in kwargs:
+            row = self.sql_session.query(self.session_identifier_action).get(kwargs['session_identifier_action_id'])
+            row.action = kwargs['action']
+            try:
+                self.sql_session.commit()
+                return True
+            except SQLAlchemyError as mysql_error:
+                print(mysql_error.__str__())
+                return False
+        else:
+            return False  # 400 Bad Request
+
+    @staticmethod
+    def delete():
+        return False  # 400 Bad Request
+
+
+class StableIdentifierRecord:
+
+    def __init__(self, sql_alchemy_base, sql_alchemy_engine):
+        self.stable_identifier_record = sql_alchemy_base.classes.stable_identifier_record
+        self.sql_session = SqlSession(sql_alchemy_engine)
+
+    def __del__(self):
+        self.sql_session.close()
+
+    def get(self, **kwargs):
+        if 'stable_identifier_record_id' in kwargs:
+            result = self.sql_session.query(self.stable_identifier_record).get(kwargs['stable_identifier_record_id'])
+            return result.stable_identifier, result.status, result.feature_type
+        else:
+            return False  # 400 Bad Request
+
+    def post(self, **kwargs):
+        if 'stable_identifier' in kwargs and 'status' in kwargs and 'feature_type' in kwargs:
+            new_stable_identifier_record = self.stable_identifier_record(stable_identifier=kwargs['stable_identifier'],
+                                                                         status=kwargs['status'],
+                                                                         feature_type=kwargs['feature_type'])
+            self.sql_session.add(new_stable_identifier_record)
+            try:
+                self.sql_session.commit()
+            except SQLAlchemyError as mysql_error:
+                print(mysql_error.__str__())
+                return False
+            return new_stable_identifier_record.stable_identifier_record_id
+        else:
+            return False  # 400 Bad Request
+
+    def patch(self, **kwargs):
+        if 'stable_identifier_record_id' in kwargs and 'status' in kwargs:
+            row = self.sql_session.query(self.stable_identifier_record).get(kwargs['stable_identifier_record_id'])
+            row.action = kwargs['status']
+            try:
+                self.sql_session.commit()
+                return True
+            except SQLAlchemyError as mysql_error:
+                print(mysql_error.__str__())
+                return False
+        else:
+            return False  # 400 Bad Request
+
+    @staticmethod
+    def delete():
+        return False  # 400 Bad Request
---
+++ session_service/tests/test_rest_api.py
@@ -0,0 +1,132 @@
+import unittest
+import configparser
+import pymysql.cursors
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
+import rest_api
+
+
+class SetUpTestDatabase:
+
+    def __init__(self):
+        config_file = '../session_service.conf'
+        config = configparser.ConfigParser()
+        config.read(config_file)
+        self.db_name = config['DataBase']['db_name']
+        self.db_host = config['DataBase']['db_host']
+        self.db_user = config['DataBase']['db_user']
+        self.db_pass = config['DataBase']['db_pass']
+        self.connection = pymysql.connect(host=self.db_host, user=self.db_user, password=self.db_pass, db=self.db_name,
+                                          charset='utf8mb4', cursorclass=pymysql.cursors.DictCursor)
+        self.database_url = "mysql+pymysql://{}:{}@{}/{}".format(self.db_user, self.db_pass, self.db_host, self.db_name)
+
+    def truncate_database(self):
+        with self.connection.cursor() as cursor:
+            sql = """SET FOREIGN_KEY_CHECKS = 0;
+                truncate table stable_identifier_record;
+                truncate table session;
+                truncate table assigning_application;
+                truncate table session_identifier_action;
+                truncate table production_database;
+                SET FOREIGN_KEY_CHECKS = 1;"""
+            for sql_line in sql.splitlines():
+                cursor.execute(sql_line)
+                self.connection.commit()
+
+
+class RestTestCase(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        test_database = SetUpTestDatabase()
+        test_database.truncate_database()
+        cls.base = automap_base()
+        cls.engine = create_engine(test_database.database_url)
+        cls.base.prepare(cls.engine, reflect=True)
+        cls.session = Session(cls.engine)
+
+    def test_AssigningApplication(self):
+        assigning_application = rest_api.AssigningApplication(self.base, self.engine)
+        application_id = assigning_application.post(name='test_application_01', version=1, description='this is a test application')
+        name, version, description = assigning_application.get(application_id=application_id)
+        self.assertEqual(('test_application_01', 1, 'this is a test application'), (name, version, description))
+        application_id_2 = assigning_application.get(name=name, version=version)
+        self.assertEqual(application_id, application_id_2)
+        result_4 = assigning_application.get(application_id=10000)
+        self.assertEqual(False, result_4)
+        result_1 = assigning_application.patch(application_id=application_id, description='update to description')
+        self.assertEqual(True, result_1)
+        result_2 = assigning_application.patch(application_id=10000, description='update to description')
+        self.assertEqual(False, result_2)
+        application_id_3 = assigning_application.post(name='test_application_01', version=1, description='this is a test application')
+        self.assertEqual(False, application_id_3)
+        result_3 = assigning_application.delete()
+        self.assertEqual(False, result_3)
+
+    def test_ProductionDatabase(self):
+        production_database = rest_api.ProductionDatabase(self.base, self.engine)
+        production_database_id = production_database.post(name='core_test_database')
+        database_name = production_database.get(production_database_id=production_database_id)
+        self.assertEqual('core_test_database', database_name)
+        database_name_2 = production_database.get(production_database_id=100000)
+        self.assertEqual(False, database_name_2)
+        result_1 = production_database.patch(production_database_id=production_database_id, name='new_data_base_name')
+        self.assertEqual(True, result_1)
+        result_2 = production_database.patch(production_database_id=100000, name='new_data_base_name')
+        self.assertEqual(False, result_2)
+        result_3 = production_database.post(name='new_data_base_name')
+        self.assertEqual(False, result_3)
+        result_4 = production_database.delete()
+        self.assertEqual(False, result_4)
+
+    def test_Session(self):
+        assigning_application = rest_api.AssigningApplication(self.base, self.engine)
+        application_id = assigning_application.post(name='test_application_02', version=1, description='this is a test application')
+        production_database = rest_api.ProductionDatabase(self.base, self.engine)
+        production_database_id = production_database.post(name='core_test_database_02')
+        session_table = rest_api.Session(self.base, self.engine)
+        session_id = session_table.post(application_id=application_id, production_database_id=production_database_id, osid_idsetid=None, message="this is a session test")
+        result, _, _, _, _, _ = session_table.get(session_id=session_id)
+        self.assertEqual(application_id, result)
+        _ = session_table.post(application_id=application_id, production_database_id=production_database_id, osid_idsetid=1, message="this is a second session test")
+        #session_id_02 = session_table.post(application_id=application_id, production_database_id=production_database_id, osid_idsetid=1, message="this is a test")
+        #self.assertEqual(False, session_id_02)
+        result_1 = session_table.patch(session_id=session_id, data_check='pass')
+        self.assertEqual(True, result_1)
+        result_3 = session_table.delete()
+        self.assertEqual(False, result_3)
+
+    def test_StableIdentifierRecord(self):
+        stable_identifier_record = rest_api.StableIdentifierRecord(self.base, self.engine)
+        stable_identifier_record_id = stable_identifier_record.post(stable_identifier='AGAP000001', status='current',
+                                                                    feature_type='gene')
+        stable_identifier, status, feature_type = stable_identifier_record.get(stable_identifier_record_id=stable_identifier_record_id)
+        self.assertEqual(('AGAP000001', 'current', 'gene'), (stable_identifier, status, feature_type))
+        result_1 = stable_identifier_record.patch(stable_identifier_record_id=stable_identifier_record_id, status='obsolete')
+        self.assertEqual(True, result_1)
+        result_2 = stable_identifier_record.delete()
+        self.assertEqual(False, result_2)
+
+    def test_session_identifier_action(self):
+        session_identifier_action = rest_api.SessionIdentifierAction(self.base, self.engine)
+        stable_identifier_record = rest_api.StableIdentifierRecord(self.base, self.engine)
+        stable_identifier_record_id = stable_identifier_record.post(stable_identifier='AGAP000002', status='current', feature_type='gene')
+        assigning_application = rest_api.AssigningApplication(self.base, self.engine)
+        application_id = assigning_application.post(name='test_application_05', version=1, description='this is a test application')
+        production_database = rest_api.ProductionDatabase(self.base, self.engine)
+        production_database_id = production_database.post(name='core_test_database_05')
+        session_table = rest_api.Session(self.base, self.engine)
+        session_id = session_table.post(application_id=application_id, production_database_id=production_database_id, osid_idsetid=None, message="this is a action test")
+        session_identifier_action_id = session_identifier_action.post(stable_identifier_record_id=stable_identifier_record_id, session_id=session_id,
+                                                                      action='create')
+        record_id, ses_id, action = session_identifier_action.get(session_identifier_action_id=session_identifier_action_id)
+        self.assertEqual((stable_identifier_record_id, session_id, 'create'), (record_id, ses_id, action))
+        result_1 = session_identifier_action.patch(session_identifier_action_id=session_identifier_action_id, action='delete')
+        self.assertEqual(True, result_1)
+        result_2 = session_identifier_action.delete()
+        self.assertEqual(False, result_2)
+
+
+if __name__ == '__main__':
+    unittest.main()
